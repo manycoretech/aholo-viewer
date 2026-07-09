282f063ce919ae7f0dd10af2bda3e1600a3e5e84
Norgerman <xyn0410@gmail.com>
2026-07-09 16:07:05 +0800

    feat(splat-transform): add future wait policy

--- packages/splat-transform-native/source/include/future_helpers.h
+++ packages/splat-transform-native/source/include/future_helpers.h
@@ -26,7 +26,15 @@ template<future_range T>
 using future_range_value_t = future_value<std::ranges::range_value_t<std::remove_cvref_t<T>>>::type;
 } // namespace detail
 
-template<detail::future_range T>
+struct FailurePolicyDrainAll;
+// Stops draining after the first observed failure. This does not cancel remaining
+// work; use only when unfinished tasks do not depend on this stack frame.
+struct FailurePolicyStopOnFirstFailure;
+
+template<typename T>
+concept future_failure_policy = std::same_as<T, FailurePolicyDrainAll> || std::same_as<T, FailurePolicyStopOnFirstFailure>;
+
+template<future_failure_policy P = FailurePolicyDrainAll, detail::future_range T>
     requires std::same_as<detail::future_range_value_t<T>, void>
 void drain_futures(T&& futures) {
     std::exception_ptr first_exception;
@@ -37,6 +45,10 @@ void drain_futures(T&& futures) {
             if (!first_exception) {
                 first_exception = std::current_exception();
             }
+
+            if constexpr (std::is_same_v<P, FailurePolicyStopOnFirstFailure>) {
+                break;
+            }
         }
     }
 
@@ -45,7 +57,34 @@ void drain_futures(T&& futures) {
     }
 }
 
-template<detail::future_range T, typename F>
+template<future_failure_policy P = FailurePolicyDrainAll, detail::future_range T, typename F>
+    requires std::same_as<detail::future_range_value_t<T>, void> &&
+             std::invocable<F&, size_t>
+void drain_futures(T&& futures, F&& f) {
+    std::exception_ptr first_exception;
+    size_t index = 0;
+    for (auto& future : futures) {
+        try {
+            future.get();
+            std::invoke(f, index);
+        } catch (...) {
+            if (!first_exception) {
+                first_exception = std::current_exception();
+            }
+
+            if constexpr (std::is_same_v<P, FailurePolicyStopOnFirstFailure>) {
+                break;
+            }
+        }
+        index++;
+    }
+
+    if (first_exception) {
+        std::rethrow_exception(first_exception);
+    }
+}
+
+template<future_failure_policy P = FailurePolicyDrainAll, detail::future_range T, typename F>
     requires(!std::same_as<detail::future_range_value_t<T>, void>) &&
             std::invocable<F&, detail::future_range_value_t<T>&&>
 void drain_futures(T&& futures, F&& f) {
@@ -57,6 +96,9 @@ void drain_futures(T&& futures, F&& f) {
             if (!first_exception) {
                 first_exception = std::current_exception();
             }
+            if constexpr (std::is_same_v<P, FailurePolicyStopOnFirstFailure>) {
+                break;
+            }
         }
     }
 
@@ -65,7 +107,7 @@ void drain_futures(T&& futures, F&& f) {
     }
 }
 
-template<detail::future_range T, typename F>
+template<future_failure_policy P = FailurePolicyDrainAll, detail::future_range T, typename F>
     requires(!std::same_as<detail::future_range_value_t<T>, void>) &&
             std::invocable<F&, detail::future_range_value_t<T>&&, size_t>
 void drain_futures(T&& futures, F&& f) {
@@ -77,6 +119,10 @@ void drain_futures(T&& futures, F&& f) {
         } catch (...) {
             if (!first_exception) {
                 first_exception = std::current_exception();
+            }
+
+            if constexpr (std::is_same_v<P, FailurePolicyStopOnFirstFailure>) {
+                break;
             }
         }
         index++;
