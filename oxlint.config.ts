a67bae8bcc341797d108147eb67f6345d0ba87ae
Norgerman <xyn0410@gmail.com>
2026-08-21 11:22:41 +0800

    chore: upgrade packages

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -33,6 +33,13 @@ export default defineConfig({
                 allowTernary: true,
             },
         ],
+        'no-unused-vars': [
+            'error',
+            {
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
+            },
+        ],
         'unicorn/no-new-array': 'off',
         'no-extra-boolean-cast': 'off',
         'typescript/no-this-alias': 'off',
